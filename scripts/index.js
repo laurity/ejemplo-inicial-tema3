479f8908f1b0cd2694cbcfce34e028842885f454
Laura López <lauralopez166@gmail.com>
2023-11-06 08:54:42 +0100

    avances

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const abrirNuevaVentana = () =>{
+    window.open("https://www.tuniversformacion.es", "_blank"); //Abre una nueva ventana
+};
+
+const abrirMismaVentana = () =>{
+    window.open("https://www.tuniversformacion.es", "_self"); //Abre en la misma ventana
+};
